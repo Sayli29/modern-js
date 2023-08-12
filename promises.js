a753338332ad898a326242dc6e21968c21365f4c
Sayli29 <sayli.tawhare@gmail.com>
2023-08-12 06:39:10 +0530

    refactor: added some example in promise file

--- promises.js
+++ promises.js
@@ -5,6 +5,12 @@ console.log("Promises in JS");
 //It is used to find out if the asynchronous operation is successfully completed or not.
 
 
+// A promise may have one of three states.
+
+// Pending
+// Fulfilled
+// Rejected
+
 
 //To create a promise object, we use the Promise() constructor.
 //  let promise = new Promise(function(resolve, reject){
@@ -12,9 +18,9 @@ console.log("Promises in JS");
 //  });
 
 
-const count = true;
+const count1 = true;
 let countValue = new Promise(function(resolve, reject){
-        if(count){
+        if(count1){
             resolve("There is count value");
         }
         else{
@@ -22,3 +28,46 @@ let countValue = new Promise(function(resolve, reject){
         }
 });
 console.log(countValue);    //Promise { 'There is count value' }
+
+
+
+//Promise Chaining
+// The then() method is used with the callback when the promise is successfully fulfilled or resolved
+
+
+const count2 = true;
+let countVal = new Promise(function(resolve, reject){
+    resolve("promise resolved");
+});
+
+console.log(countVal);      //Promise { 'Promise resolved' }
+countVal
+    .then(function res1(val1){
+        console.log(val1);
+    })
+    .then(function res2(){
+        console.log("multiple function can be called this way");
+    })
+
+
+
+//promise with catch and finally
+const const3 = false;
+let countVal3 = new Promise(function(resolve, reject){
+    reject("Promise rejected");
+});
+
+
+countVal3
+    //executes when promise is resolved successfully
+    .then(function res3(){
+        console.log("resolved successfully");
+    })
+
+    .catch(function errorval(result){
+        console.log(result);
+    })
+
+    .finally(function end(){
+        console.log("the execution ends here");
+    })
